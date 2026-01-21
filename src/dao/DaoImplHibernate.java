32483e715884859ead598775a5fe577dcba156dd
John Henard Salango Fernandez <henardsalango09@gmail.com>
2026-01-21 17:47:01 +0100

    Implemented #23 addProduct, updateProduct and deleteProduct Hibernate

--- src/dao/DaoImplHibernate.java
+++ src/dao/DaoImplHibernate.java
@@ -3,6 +3,7 @@ package dao;
 import java.util.ArrayList;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -80,20 +81,67 @@ public class DaoImplHibernate implements Dao {
 
 	@Override
 	public boolean addProduct(Product product) {
-		// TODO Auto-generated method stub
-		return false;
+		connect();
+		
+		Transaction transaction = null;
+		try {
+			transaction = session.beginTransaction();
+			
+			session.save(product);
+			
+			transaction.commit();
+			return true;
+		} catch (Exception e) {
+			if (transaction != null) transaction.rollback();
+			
+			System.out.println("Error: couldn't add Product Hibernate");
+			return false;
+		}
 	}
 
 	@Override
 	public boolean updateProduct(Product product) {
-		// TODO Auto-generated method stub
-		return false;
+		connect();
+		
+		Transaction transaction = null;
+		try {
+			transaction = session.beginTransaction();
+			
+			session.update(product);
+			
+			transaction.commit();
+			return true;
+		} catch (Exception e) {
+			if (transaction != null) transaction.rollback();
+			
+			System.out.println("Error: couldn't update Product Hibernate");
+			return false;
+		}
 	}
 
 	@Override
 	public boolean deleteProduct(int id) {
-		// TODO Auto-generated method stub
-		return false;
+		connect();
+		
+		Transaction transaction = null;
+		try {
+			transaction = session.beginTransaction();
+			
+			Product product = session.get(Product.class, id);
+			
+			if (product != null) {
+				session.delete(product);
+				transaction.commit();
+				return true;
+			}
+			
+			return false;
+		} catch (Exception e) {
+			if (transaction != null) transaction.rollback();
+			
+			System.out.println("Error: couldn't delete Product Hibernate");
+			return false;
+		}
 	}
 
 }
